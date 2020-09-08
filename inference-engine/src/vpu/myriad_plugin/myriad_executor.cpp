4a0db54a3b73c74fe0befb5915bf24cd2bf520ea
DariaMityagina <daria.mityagina@intel.com>
2020-09-08 15:58:27 +0300

    compilation without a connected device with no usage of deprecated option

--- inference-engine/src/vpu/myriad_plugin/myriad_executor.cpp
+++ inference-engine/src/vpu/myriad_plugin/myriad_executor.cpp
@@ -261,13 +261,11 @@ DevicePtr MyriadExecutor::openDevice(std::vector<DevicePtr>& devicePool,
             });
 
         // Return mock device. If try infer with it, exception will be thrown
-        if (availableDevices.empty() && config.platform() != NC_ANY_PLATFORM) {
+        if (availableDevices.empty()) {
             DeviceDesc device;
             device._platform = config.platform();
             device._protocol = config.protocol();
             return std::make_shared<DeviceDesc>(device);
-        } else if (availableDevices.empty()) {
-            THROW_IE_EXCEPTION << "Can not init Myriad device: " << ncStatusToStr(nullptr, booted);
         }
 
         auto deviceWithMinExecutors = std::min_element(availableDevices.begin(), availableDevices.end(),
