2dd1ab2b9ab7fbd5070823e435211e61ab8b9332
DariaMityagina <daria.mityagina@intel.com>
2021-07-29 15:44:00 +0300

    [VPU] switch CNNLayer -> ngraph - tile fixes

--- inference-engine/src/vpu/graph_transformer/src/stages/tile.cpp
+++ inference-engine/src/vpu/graph_transformer/src/stages/tile.cpp
@@ -77,81 +77,94 @@ struct TileParams {
     size_t axis;
     size_t tiles;
 };
-TileParams getAxisAndTiles (const NodePtr& node) {
-        auto tile = std::dynamic_pointer_cast<ngraph::opset1::Tile> (node);
-        if (!tile) {
+bool getAxisAndTiles(const NodePtr& node, int& axis_par, int& tiles_par) {
+    auto tile = std::dynamic_pointer_cast<ngraph::opset1::Tile>(node);
+    if (!tile) {
+        return false;
+    }
+
+    auto tiles_node = std::dynamic_pointer_cast<ngraph::opset1::Constant>(tile->input_value(1).get_node_shared_ptr());
+    if (!tiles_node) return false;
+
+    auto tiles = tiles_node->cast_vector<int64_t>();
+    auto input_shape_rank = static_cast<size_t>(tile->get_input_partial_shape(0).rank().get_length());
+    int64_t cur_dim_id = tiles.size() - 1;
+
+    if (static_cast<int64_t>(tiles.size()) != input_shape_rank) return false;
+
+    auto last_node = tile->input_value(0);
+    auto friendly_name = tile->get_friendly_name();
+
+    int num_of_tile_dims = 0;
+    for (auto t : tiles) {
+        if (t != 1) {
+            num_of_tile_dims++;
         }
+    }
 
-        auto tiles_node = std::dynamic_pointer_cast<ngraph::opset1::Constant> (tile->input_value(1).get_node_shared_ptr());
-        if (!tiles_node) {
-        }
-
-        auto tiles = tiles_node->cast_vector<int64_t>();
-        auto input_shape_rank = tile->get_input_partial_shape(0).rank().get_length();
-        int64_t cur_dim_id = tiles.size() - 1;
-
-        IE_ASSERT(!(static_cast<int64_t>(tiles.size()) != input_shape_rank));
-
-        // IE Tile operations supports only one axis to be tiled
-        // bool already_set = false;
-        // int64_t axis, tiles;
-        // for (size_t i = 0; i < input_shape.size(); ++i) {
-        //     if (shape[i] != 1) {
-        //         if (already_set) return false;
-        //         axis = i;
-        //         tiles = shape[i];
-        //         already_set = true;
-        //     }
-        // }
-        //
-        // if (!already_set) return false;
-        auto last_node = tile->input_value(0);
-        auto friendly_name = tile->get_friendly_name();
-
-        int num_of_tile_dims = 0;
-        for (auto t : tiles) {
-            if (t != 1) {
-                num_of_tile_dims++;
+    if (num_of_tile_dims == 0) {
+        auto outputs = tile->get_output_target_inputs(0);
+        for (const auto &out : outputs) {
+            if (std::dynamic_pointer_cast<ngraph::opset1::Result>(out.get_node()->shared_from_this())) {
+                return false;
             }
         }
-        // Will generate sequence of Tile operations if num_of_tile_dims != 1
-        // because IE Tile operations supports only one axis to be tiled.
-        // To keep op name unique will use special IE specific delimiter ':'
-        // Original frameworks doesn't use such delimiter in names, so it will
-        // guarantee that newly generated name like "original_name:_1" doesn't
-        // match with already existed names.
-        if (num_of_tile_dims > 1) {
-            friendly_name += ":";
-        }
+        ngraph::replace_node(tile, {last_node});
+        return true;
+    }
 
-        ngraph::NodeVector new_ops;
-        TileParams outTileParams;
-        auto tiles_it = tiles.rbegin();
-        while (tiles_it != tiles.rend()) {
-            int64_t tile_dim = *tiles_it;
-            if (tile_dim != 1) {
-                outTileParams.axis = cur_dim_id;
-                outTileParams.tiles = tile_dim;
-            }
-            --cur_dim_id;
-            ++tiles_it;
+    // Will generate sequence of Tile operations if num_of_tile_dims != 1
+    // because IE Tile operations supports only one axis to be tiled.
+    // To keep op name unique will use special IE specific delimiter ':'
+    // Original frameworks doesn't use such delimiter in names, so it will
+    // guarantee that newly generated name like "original_name:_1" doesn't
+    // match with already existed names.
+    if (num_of_tile_dims > 1) {
+        friendly_name += ":";
+    }
+
+    ngraph::NodeVector new_ops;
+
+    auto tiles_it = tiles.rbegin();
+    while (tiles_it != tiles.rend()) {
+        int64_t tile_dim = *tiles_it;
+        if (tile_dim != 1) {
+            std::vector<int64_t> dims(input_shape_rank, 1);
+            dims[cur_dim_id] = tile_dim;
+            auto const_node = std::make_shared<ngraph::opset1::Constant>(ngraph::element::i64, ngraph::Shape{input_shape_rank}, dims);
+            auto new_tile = std::make_shared<ngraph::opset1::Tile>(last_node, const_node);
+            new_tile->set_friendly_name(friendly_name);
+            friendly_name += "_" + std::to_string(cur_dim_id);
+            new_ops.push_back(new_tile);
+            axis_par = cur_dim_id;
+            tiles_par = tile_dim;
+
+            last_node = new_tile;
         }
-        return outTileParams;
+        --cur_dim_id;
+        ++tiles_it;
+    }
+    return true;
 }
 void FrontEnd::parseTile(const Model& model, const NodePtr& node, const DataVector& inputs, const DataVector& outputs) const {
     const auto& tile = ngraph::as_type_ptr<ngraph::opset4::Tile>(node);
     VPU_THROW_UNLESS(tile != nullptr, "Can't parse node with name %s and type %s. Node is nullptr", node->get_friendly_name(), node->get_type_name());
-    auto params = getAxisAndTiles(tile);
-    IE_ASSERT(inputs.size() == 1);
-    IE_ASSERT(outputs.size() == 1);
+    int axis_p = 0, tile_p = 0;
+    TileParams params;
+    getAxisAndTiles(tile, axis_p, tile_p);
+
+    params.axis = axis_p;
+    params.tiles = tile_p;
 
     auto input = inputs[0];
     auto output = outputs[0];
+    printf("params.axis = %d\n\n", int(params.axis));
+    printf("params.tiles = %d\n\n", int(params.tiles));
 
     IE_ASSERT(params.axis < input->desc().numDims());
 
     auto perm = DimsOrder::fromNumDims(input->desc().numDims()).toPermutation();
-    auto axis = perm[input->desc().numDims() - 1 -  params.axis];
+    auto axis = perm[input->desc().numDims() - 1 - params.axis];
 
     auto stage = model->addNewStage<TileStage>(tile->get_friendly_name(), StageType::Tile, tile, {input}, {output});
     stage->attrs().set("axis", axis);
